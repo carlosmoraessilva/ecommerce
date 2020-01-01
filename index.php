46583eb8f4a75911f810f7b94038af5feda58dbd
Carlos Moraes <carlosm1412@gmail.com>
2020-01-01 11:40:30 -0300

    tela de login

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 require_once("vendor/autoload.php");
 
+session_start();
+
+
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,11 +24,42 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
-	$page = new PageAdmin();
-	
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
+
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
 
 $app->run();
 
